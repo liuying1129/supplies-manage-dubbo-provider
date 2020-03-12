fc41e47d2f4f3eb561c2fc015934f1b90af8b9a6
liuying <liuying@baibu.la>
2020-03-12 21:19:54 +0800

    3333333333333

--- src/main/java/com/yklis/suppliesmanage/provider/service/impl/SuppliesManageServiceImpl.java
+++ src/main/java/com/yklis/suppliesmanage/provider/service/impl/SuppliesManageServiceImpl.java
@@ -3,6 +3,8 @@ package com.yklis.suppliesmanage.provider.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yklis.lisfunction.service.ExecSQLCmdService;
 import com.yklis.lisfunction.service.SelectDataSetSQLCmdService;
 import com.yklis.suppliesmanage.inf.SuppliesManageService;
 
@@ -11,11 +13,20 @@ public class SuppliesManageServiceImpl implements SuppliesManageService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private SelectDataSetSQLCmdService selectDataSetSQLCmdService;    
+    private SelectDataSetSQLCmdService selectDataSetSQLCmdService;
+    
+    private ExecSQLCmdService execSQLCmdService;
 
     @Override
 	public String queryNoAuditReceiptList() {
 		
-    	return selectDataSetSQLCmdService.selectDataSetSQLCmd("select * from SJ_RK_Fu");
+    	return selectDataSetSQLCmdService.selectDataSetSQLCmd("select * from SJ_RK_Fu where Audit_Date is null");
 	}
+    
+    @Override
+    public String deleteReceipt(String unid) {
+    	
+    	return execSQLCmdService.ExecSQLCmd("delete from SJ_RK_Fu where unid="+unid);
+    }
+
 }
