f02aca1ae673e7e75715086ec2711ec97c21dbd9
liuying <liuying@baibu.la>
2020-03-22 10:55:18 +0800

    增加Windows自动服务工具

---
+++ winservice.bat
@@ -0,0 +1,6 @@
+D:\source_reference\supplies-manage-dubbo-provider\WinSW.NET4.exe install
+net start supplies-manage-dubbo-provider
+::net stop yklisschedule
+::D:\source_reference\SpringBoot-Schedule\WinSW.NET4.exe uninstall
+
+pause
